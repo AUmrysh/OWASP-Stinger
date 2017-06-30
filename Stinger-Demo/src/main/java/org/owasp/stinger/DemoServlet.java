c63d26f0dca01491349f312da95e959e3e49468b
chad <chobbs@caci.com>
2017-06-30 12:04:08 -0400

    Turning things into servlets instead of raw links

---
+++ Stinger-Demo/src/main/java/org/owasp/stinger/DemoServlet.java
@@ -0,0 +1,34 @@
+package org.owasp.stinger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by itsallvoodoo on 6/30/17.
+ */
+
+@WebServlet("/demo")
+public class DemoServlet extends HttpServlet {
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        request.getRequestDispatcher("/demo.html").forward(request, response);
+    }
+}
